f4561064428c7106cbba4165de3e2cf861968df2
kodlend <128508450+kodlend@users.noreply.github.com>
2024-02-06 21:14:06 +0100

    Added some changes

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-const header = document.getElementById('header');
+/* const header = document.getElementById('header');
 
-// Function to handle scroll event
 function handleScroll() {
-  // Calculate how far the user has scrolled down the page
   const scrollY = window.scrollY || window.pageYOffset;
   
-  // Calculate the opacity based on the scroll position
   const opacity = 1 - (scrollY / window.innerHeight);
 
-  // Set the opacity of the header
   header.style.opacity = opacity <= 0 ? '0' : opacity.toString();
 }
 
-// Add scroll event listener to the window
-window.addEventListener('scroll', handleScroll);
+window.addEventListener('scroll', handleScroll); */
