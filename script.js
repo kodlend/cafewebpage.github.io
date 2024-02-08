43035af5bd3ec22ef98d0ab13c0553bca72b60e0
kodlend <128508450+kodlend@users.noreply.github.com>
2024-02-08 12:15:30 +0100

    added files to branch1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /* const header = document.getElementById('header');
 
 function handleScroll() {
+<<<<<<< Updated upstream
   const scrollY = window.scrollY || window.pageYOffset;
   
   const opacity = 1 - (scrollY / window.innerHeight);
@@ -9,3 +10,17 @@ function handleScroll() {
 }
 
 window.addEventListener('scroll', handleScroll); */
+=======
+  
+  const scrollY = window.scrollY || window.pageYOffset;
+  
+  
+  const opacity = 1 - (scrollY / window.innerHeight);
+
+
+  header.style.opacity = opacity <= 0 ? '0' : opacity.toString();
+}
+
+
+window.addEventListener('scroll', handleScroll);
+>>>>>>> Stashed changes
